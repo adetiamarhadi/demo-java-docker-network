18d90b6b913fada0f0e3d17cd10459c40c8eade3
adetiamarhadi <adetia.marhadi@gmail.com>
2020-04-16 11:47:16 +0700

    java docker networking mongo and redis

---
+++ src/main/java/com/example/demodockernetwork/DemoDockerNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodockernetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demodockernetwork/DemoDockerNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodockernetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
